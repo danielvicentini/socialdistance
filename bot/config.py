6720406744621df10e6c6215af86667ae6105c06
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2020-06-27 22:51:41 -0300

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,27 @@
-#import os
-from config_shared import bot_token, bot_email, bot_webhook, bot_tag
+# -*- coding: utf-8 -*-
 
-# Ponha aqui os dados do seu robozinho
+# import vars from config_shared 
+# bot info
+# admins info
+
+from config_shared import bot_token, bot_email, bot_webhook, bot_tag, admins
+
+# DONT TOUCH THIS VARS - THEY'RE GET FROM config_shared.py
+
 bottoken=bot_token
-#exemplo='Yia0dsfdsfsdf...' - tirado do site developer.webex.com
 botmail=bot_email
-#exemplo='mybotmail@spark.io' - tirado do site developer.webex.com
-
-# Ponha aqui os dados do seu Webhook
-# webhook_url o endereco publico onde esta o app
 webhook_url=bot_webhook
-# exemplo webhook_url="https://mywebhook.com"
-# webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
 webhook_name=bot_tag
-# exemplo webhook_name="meuprimeirochatbot"
 
 # Novidade 27.4.20
 
-#variaveis globais
-#memoriza comandos
+#global vars
 memoria={}
 
 
-# Config para teste
+# Sample inventory
 configuracao ={
-	"versao": "1",
+	"version": "1",
 	"inventario": [{
 			"sala": "Cafeteria",
 			"access-point": "AP-1",
@@ -36,21 +33,13 @@ configuracao ={
 			"distancia": 30 }]}
 
 # nova config 8.6.20
-# PIN = senha para comitar alteraçoes
-# admin = lista de pessoas que podem aplicar config
-# max = max pessoas numa sala/andar, interval = periodo de coleta de dados
-# distance True/False = liga desliga análise
-# mask True/False = liga desliga análise de mascara
+# admin = list of admins
+# max = max people per rooms
 # 
 configa= {
 	"versao": "1",
-	"PIN": "2504",
-	"admin":"flcorrea,acassemi,dvicenti,maralves,aluciade,lpavanel",
+	"admin":admins ,
 	"data": {
-		"max":30,
-		"interval":15,
-		"distance": False,
-		"mask": False,
-		"tracing": False
+		"max":30
 	}
 }
