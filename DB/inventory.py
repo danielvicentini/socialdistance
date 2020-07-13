3b4e01f6e47118068803f5add7ae7fd9afd21a69
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2020-07-13 15:07:22 -0300

    Add files via upload

--- DB/inventory.py
+++ DB/inventory.py
@@ -3,21 +3,21 @@ DeviceInventory = [
    "SerialNumber": "Q2HV-9RTC-ZWMN",
    "Device": "Camera",
    "Nome": "Camera do Andrey",
-   "Location": "Sala",
+   "Location": "Sala de Entrada",
    "NetworkID": "L_711568741124547888"
    },
   {
    "SerialNumber": "Q2FV-GA8U-S45S",
    "Device": "Camera",
    "Nome": "MV12 Sala",
-   "Location": "Sala",
+   "Location": "Sala 1",
    "NetworkID": "L_711568741124547888"
    },
   {
    "SerialNumber": "MR33_Sala",
    "Device": "WiFi",
    "Nome": "MR33_Sala",
-   "Location": "Sala",
+   "Location": "Sala 1",
    "NetworkID": "L_711568741124547888"
   },
   {
@@ -39,16 +39,41 @@ DeviceInventory = [
    "Device": "WiFi",
    "Nome": "MR18_Quartos",
    "Location": "Quartos",
-   "NetowrkID": "L_711568741124547888"
+   "NetworkID": "L_711568741124547888"
   },
   {
   "SerialNumber": "Q2GV-7HEL-HC6C",
   "Device": "Camera",
   "Nome": "MV_Sandbox",
   "Location": "Sandbox",
-  "NetowrkID": "L_711568741124547888"
+  "NetworkID": "L_711568741124547888"
   },
 ]
+
+
+def getDeviceInfoName (item,Nome):
+  # Devole o valor de uma chave, após encontrar o nome do device correspondente
+  for device in DeviceInventory:
+    if device["Nome"].lower() == Nome.lower():
+      try:
+        itemData = device[item]
+        return itemData
+      except:
+        return "nodata"
+  return "erro"
+
+
+def getDeviceInfoSerial (item,serialnumber):
+  # Devole o valor de uma chave, após encontrar o Serial correspondente
+  for device in DeviceInventory:
+    if device["SerialNumber"].lower() == serialnumber.lower():
+      try:
+        itemData = device[item]
+        return itemData
+      except:
+        return "nodata"
+  return "erro"
+
 
 def DeviceInfo (serialnumber):
   for device in DeviceInventory:
