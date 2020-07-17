32e2908703f0aa82e3b9a7bf095f521b8421c9e5
Ana Lucia de Faria (aluciade) <aluciade@cisco.com>
2020-07-17 09:59:56 -0300

    Adding Occupancy Report (not ready)

--- DB/bdFluxQueries.py
+++ DB/bdFluxQueries.py
@@ -1,6 +1,40 @@
+import sys
+import os
+sys.path.append(os.path.abspath('..'))
+
 from influxdb_client import InfluxDBClient
 from config_shared import INFLUXDB_HOST, INFLUXDB_PORT, INFLUXDB_DBUSER, INFLUXDB_DBPASSWORD, INFLUXDB_DBNAME, INFLUXDB_USER, INFLUXDB_PASSWORD
 from config_shared import TABELA_MV, TABELA_TOTAL, TABELA_TRACE
+
+
+
+LocationInventory = [
+  {
+   "Location": "Cozinhha",
+   "Occupancy": 1
+   },
+  {
+   "Location": "EDU",
+   "Occupancy": 2
+   },
+  {
+   "Location": "IND",
+   "Occupancy": 2
+   },
+  {
+   "Location": "Quartos",
+   "Occupancy": 2
+   },
+  {
+   "Location": "Sala",
+   "Occupancy": 2
+   },
+  {
+   "Location": "Sandboz",
+   "Occupancy": 3
+   }
+]
+
 
 
 QUERY_TRACE = \
@@ -43,6 +77,7 @@ QUERY_TRACE = \
 # Input:
 #    targetUser = userid to be traced
 #    startTime = string with initial time for tracing (ex: "2020-07-14T00:00:00Z")
+#    stopTime = string with final time for tracing (ex: "2020-07-14T00:00:00Z")
 #
 # Output: List of dictionaty entries in the format
 #    {
@@ -55,6 +90,56 @@ def TraceReport(targetUser, startTime, stopTime):
   # Connects to the database
   url = "http://{}:{}".format(INFLUXDB_HOST, INFLUXDB_PORT)     
   client = InfluxDBClient(url=url, token="", org="")
+
+  # Build trace query
+  query = QUERY_TRACE % (targetUser, startTime, stopTime, TABELA_TRACE)
+  tables = client.query_api().query(query)
+  client.__del__()
+
+  # Query result is a list of all tables creted in TRACE_QUERY, each of them of type FluxTable
+  # see https://github.com/influxdata/influxdb-client-python/blob/master/influxdb_client/client/flux_table.py#L5 for more info
+
+  # Build result as a list of all found records in suspecTable
+  result =[]
+  for table in tables:
+    for record in table.records:
+      subdict = {x: record.values[x] for x in ['local','userid']}
+      if subdict not in result:
+        result.append(subdict)
+  return result
+
+
+
+#
+# Distancing report
+#
+# Input:
+#    startTime = string with initial time for report (ex: "2020-07-14T00:00:00Z")
+#
+# Output: List of dictionaty entries in the format
+#    {
+#     'local': <local where user met targetUser>, 
+#     'userid': <id of user that met targerUser>'
+#    }
+#
+def OccupancyReport(startTime):
+
+  # Connects to the database
+  url = "http://{}:{}".format(INFLUXDB_HOST, INFLUXDB_PORT)     
+  client = InfluxDBClient(url=url, token="", org="")
+
+  # Preapre to create a table with maximum occupancy per room 
+  TEMP_TABLE = "MaximumOccupancy"
+  write_api = client.write_api(write_options=SYNCHRONOUS)
+
+  #Build a record per local
+  record = []
+  for location in LocationInventory:
+    _point = Point(TEMP_TABLE).tag("local", location.Location).field("occupancy", location.Occupancy)
+    record.append(_point)
+
+  # Write to temporary table
+  write_api.write(bucket=INFLUXDB_DBNAME, record=record)
 
   # Build trace query
   query = QUERY_TRACE % (targetUser, startTime, stopTime, TABELA_TRACE)
