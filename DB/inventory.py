89f997957f2f015b5b07682fab2c9d33d08816d6
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2020-07-22 16:52:44 -0300

    Update inventory.py

--- DB/inventory.py
+++ DB/inventory.py
@@ -1,54 +1,19 @@
 DeviceInventory = [
   {
-   "SerialNumber": "Q2HV-9RTC-ZWMN",
+   "SerialNumber": "XXX-YYYY-ZZZ",
    "Device": "Camera",
-   "Nome": "Camera do Andrey",
-   "Location": "Sala de Entrada",
-   "NetworkID": "L_711568741124547888"
+   "Nome": "Device Name",
+   "Location": "Device Location",
+   "NetworkID": "L_XXXXXXXXXXXXXXXXX"
    },
   {
-   "SerialNumber": "Q2FV-GA8U-S45S",
+   "SerialNumber": "XXX-YYYY-ZZZ",
    "Device": "Camera",
-   "Nome": "MV12 Sala",
-   "Location": "Sala 1",
-   "NetworkID": "L_711568741124547888"
-   },
-  {
-   "SerialNumber": "MR33_Sala",
-   "Device": "WiFi",
-   "Nome": "MR33_Sala",
-   "Location": "Sala 1",
-   "NetworkID": "L_711568741124547888"
-  },
-  {
-   "SerialNumber": "Q2FV-CWAD-KJJN",
-   "Device": "Camera",
-   "Nome": "MV12 Cozinha",
-   "Location": "Cozinha",
-   "NetworkID": "L_711568741124547888"
-  },
-  {
-   "SerialNumber": "MR24_Cozinha",
-   "Device": "WiFi",
-   "Nome": "MR24_Cozinha",
-   "Location": "Cozinha",
-   "NetworkID": "L_711568741124547888"
-  },
-  {
-   "SerialNumber": "MR18_Quartos",
-   "Device": "WiFi",
-   "Nome": "MR18_Quartos",
-   "Location": "Quartos",
-   "NetworkID": "L_711568741124547888"
-  },
-  {
-  "SerialNumber": "Q2GV-7HEL-HC6C",
-  "Device": "Camera",
-  "Nome": "MV_Sandbox",
-  "Location": "Sandbox",
-  "NetworkID": "L_711568741124547888"
-  },
-]
+   "Nome": "Device Name",
+   "Location": "Device Location",
+   "NetworkID": "L_XXXXXXXXXXXXXXXXX"
+   }
+ ]
 
 
 def getDeviceInfoName (item,Nome):
