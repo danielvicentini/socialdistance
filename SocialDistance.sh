2ec96a36bc00a7db7313a7c7ca8afb5bb7eacf68
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2020-07-22 19:42:21 -0300

    Update SocialDistance.sh

--- SocialDistance.sh
+++ SocialDistance.sh
@@ -2,10 +2,13 @@
 echo BD/report Server start
 nohup python3 report.py &
 echo Track System start
+cd track_system
 nohup python3 pysyslog.py &
 nohup python3 syslog_parsing.py &
 nohup python3 trigger.py &
+cd facemask-detection
 echo Mask Detection start
 nohup python3 app.py &
+cd..
 echo Social Bot start 
 nohup socialbot.py &
